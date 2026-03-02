290bb20e7328f31304f4bbf2f82829394a7d8701
khushi21012001 <khushi21misal@gmail.com>
2026-03-02 15:54:28 +0530

    Added multiple SQL queries for sales analysis

--- queries.sql
+++ queries.sql
@@ -1,4 +1,21 @@
--- Calculate total revenue from sales dataset
+-- 1. View all sales data
+SELECT *
+FROM UntitledspreadsheetSheet1;
 
+-- 2. Total revenue
 SELECT SUM(c4) AS total_revenue
+FROM UntitledspreadsheetSheet1;
+
+-- 3. Revenue by city
+SELECT c3 AS city, SUM(c4) AS revenue
+FROM UntitledspreadsheetSheet1
+GROUP BY c3;
+
+-- 4. Revenue by product
+SELECT c2 AS product, SUM(c4) AS revenue
+FROM UntitledspreadsheetSheet1
+GROUP BY c2;
+
+-- 5. Average product price
+SELECT AVG(c4) AS avg_price
 FROM UntitledspreadsheetSheet1;
